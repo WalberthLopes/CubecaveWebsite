93e7189be02860ed651c0cb11a9fee3b21590f36
Nexthi <contato@walberth.com>
2022-06-04 10:51:04 -0300

    Sistema de login e inicio do banco de dados

---
+++ php/index.php
@@ -0,0 +1,42 @@
+<?php
+    include_once 'header.php';
+?>
+        <div id="hover">
+            <p class="cards-container">
+                <span id="card">
+                    <span id="block-card">
+                        <span id="block-title">
+                            <span id="card-title">
+                                Bloodmoon Update, Maio de 2022
+                            </span>
+                        </span>
+                        <img id="card-banner" src="/assets/images/bloodmoon.png">
+                        <span id="block-text">
+                            <span id="card-text">
+                                A cada 25 dias do mine surgirá uma bloodmoon e você enfrentará ondas de monstros
+                                durante uma noite longa e cheia de perigos! Você consegue sair vivo dessa?
+                            </span>
+                        </span>
+                    </span>
+                    <br><br>
+                </span>
+                <span id="minicard">
+                    <span id="block-image">
+                        <img src="/assets/images/minicard.png">
+                        <span id="block-text-minicard">
+                            <span id="minicard-text">
+                                Melhore sua conta!
+                            </span>
+                            <button id="minicard-button" onclick="window.location.href='https://cubecave.lojasquare.net'">ABRIR LOJA</button>
+                        </span>
+                    </span>
+                </span>
+            </p>
+        </div>
+        <div id="footer">
+            3
+        </div>
+    </div>
+    <script src="/assets/javascript/main.js"></script>
+</body>
+</html>
